f43438b8e2d925e4188b9d44bdfb3ca36b655b72
Jeremy <tempestj@sheridancollege.ca>
2025-01-30 10:31:56 -0500

    step 1 added

---
+++ tests/tests_add.py
@@ -0,0 +1,8 @@
+import calculator
+
+def test_two_plus_two():
+    """
+    Asserts that given the parameters of 2 and 2, the add function should return 4
+    :return:
+    """
+    assert calculator.add(2,2) == 4
